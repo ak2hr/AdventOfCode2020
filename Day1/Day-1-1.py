6bec686033692a6a0707adf35ce8194eb38c6b40
Andrew Kepley <ak2hr@virginia.edu>
2020-12-01 00:15:12 -0500

    Day 1 completed

--- Day1/Day-1-1.py
+++ Day1/Day-1-1.py
@@ -1,1 +1,11 @@
-print("hello world!")
+file = open("Day1/input.txt", "r")
+
+nums = []
+for line in file:
+    nums.append(int(line))
+for x in range(len(nums)):
+    for y in range(x+1, len(nums)):
+        for z in range(y+1, len(nums)):
+            if(nums[x] + nums[y] + nums[z] == 2020):
+                print(nums[x], ",", nums[y], ",", nums[z])
+                print(nums[x] * nums[y] * nums[z])
